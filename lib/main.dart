082558e5b029cff31772be30b3eab6dd94761af6
samih damaj <samih_damaj@hotmail.com>
2021-12-12 23:42:21 +0200

    asda

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:udemy_flutter/modules/login/LoginScreen.dart';
 import 'package:udemy_flutter/modules/messanger/MessagingScreen.dart';
-=======
 import 'package:get/get.dart';
 import 'package:udemy_flutter/BMICalculator.dart';
 import 'package:udemy_flutter/Controller/BMIController.dart';
 import 'package:udemy_flutter/Controller/helper/Binding.dart';
-import 'package:udemy_flutter/MessagingScreen.dart';
->>>>>>> fe033443ca36bed03747d4062520607e89977211
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,11 +19,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       initialBinding: Binding(),
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
       home: LoginScreen(),
-=======
-      home: BMICalculator(),
->>>>>>> fe033443ca36bed03747d4062520607e89977211
     );
   }
 }
