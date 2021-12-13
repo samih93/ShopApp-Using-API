5bcaf39ffcd342f42644c707964fb47d58ccb8da
isfacademy <isfacademy.2023@gmail.com>
2021-12-13 11:58:12 +0200

    ccc

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:udemy_flutter/modules/archive_tasks/archive_task.dart';
 import 'package:udemy_flutter/modules/new_tasks/new_task_screen.dart';
 import 'package:udemy_flutter/modules/todo_tasks/todo_task.dart';
+import 'package:sqflite/sqflite.dart';
 
 class HomeLayout extends StatefulWidget {
   @override
@@ -12,6 +13,13 @@ class _HomeLayoutState extends State<HomeLayout> {
   int _currentIndex = 0;
   final screens = [NewTaskScreen(), ToDoTaskScreen(), ArchiveTaskScreen()];
   final appbar_title = ["New Tasks", "Todo Tasks", "Archive Tasks"];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    createDatabase();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +46,48 @@ class _HomeLayoutState extends State<HomeLayout> {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        child: Icon(
+          Icons.add,
+        ),
+        onPressed: () {
+          // try {
+          //   String name = await getName();
+          //   print(name);
+          //   throw ('some errorrr');
+          // } catch (error) {
+          //   print("error ${error.toString()}");
+          // }
+          getName().then((value) {
+            print(value);
+          }).catchError((error) {
+            print(error.toString());
+          });
+        },
+      ),
+    );
+  }
+
+  Future<String> getName() async {
+    return "samih damaj";
+  }
+
+  void createDatabase() async {
+    var database = await openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate: (database, version) {
+        print("database created");
+        database
+            .execute(
+                "Create Table tasks(id Integer PRIMARY KEY,title TEXT,date TEXT , time TEXT , status TEXT)")
+            .then((value) => print('table created'))
+            .catchError((onError) => print(onError.toString()));
+      },
+      onOpen: (database) {
+        print('database opened');
+      },
     );
   }
 }
