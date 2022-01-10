6bdb23e2b15f20da56753ee6a33ce541282929ae
samih damaj <samih_damaj@hotmail.com>
2022-01-10 23:36:01 +0200

    xxzc

--- lib/models/social_app/message_model.dart
+++ lib/models/social_app/message_model.dart
@@ -3,19 +3,21 @@ class SocialMessageModel {
   String? receiverId;
   String? messageDate;
   String? text;
+  String? image;
 
-  SocialMessageModel({
-    this.senderId,
-    this.receiverId,
-    this.messageDate,
-    this.text,
-  });
+  SocialMessageModel(
+      {this.senderId,
+      this.receiverId,
+      this.messageDate,
+      this.text,
+      this.image});
 
   SocialMessageModel.fromJson(Map<String, dynamic> json) {
     senderId = json['senderId'];
     receiverId = json['receiverId'];
     messageDate = json['messageDate'];
     text = json['text'];
+    image = json['image'];
   }
 
   Map<String, dynamic> toJson() {
@@ -24,6 +26,7 @@ class SocialMessageModel {
       'receiverId': receiverId,
       'messageDate': messageDate,
       'text': text,
+      'image': image,
     };
   }
 }
--- lib/modules/social_app/chat_details/chatdetailsController.dart
+++ lib/modules/social_app/chat_details/chatdetailsController.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:udemy_flutter/models/social_app/message_model.dart';
 import 'package:udemy_flutter/shared/componets/constants.dart';
 
@@ -24,7 +28,8 @@ class ChatDetailsController extends GetxController {
         senderId: uId,
         receiverId: receiverId,
         text: text,
-        messageDate: messageDate);
+        messageDate: messageDate,
+        image: _imageMessageUrl ?? '');
 
     // NOTE write message in user sender
     FirebaseFirestore.instance
@@ -81,13 +86,71 @@ class ChatDetailsController extends GetxController {
         // ! get => of type Future<QuerySnapshot> get data one time
         .listen((event) {
       _listOfMessages = []; // ! cz listen get old and new data
-      update();
+      // update();
       event.docs.forEach((element) {
         _listOfMessages.add(SocialMessageModel.fromJson(element.data()));
         update();
       });
     });
     isGetMessageSuccess.value = true;
+    update();
+  }
+
+// NOTE Picke Image
+  File? _messageImage = null;
+  File? get messageImage => _messageImage;
+
+  var picker = ImagePicker();
+  Future<void> pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _messageImage = File(pickedFile.path);
+      //NOTE :upload profile image to firebase storage
+      //uploadMessageImage();
+      update();
+    } else {
+      print('no image selected');
+    }
+  }
+
+//NOTE upload message image to database
+  bool? _isloadingUrlMessage = false;
+  bool? get isloadingUrlMessage => _isloadingUrlMessage;
+
+  String? _imageMessageUrl = null;
+  String? get imageMessageUrl => _imageMessageUrl;
+
+  Future<void> uploadMessageImage(String receiverId) async {
+    _isloadingUrlMessage = true;
+    update();
+    await FirebaseStorage.instance
+        .ref('')
+        .child('users/chats/${Uri.file(_messageImage!.path).pathSegments.last}')
+        .putFile(_messageImage!)
+        .then((value) {
+      value.ref.getDownloadURL().then((value) {
+        _imageMessageUrl = value;
+        _isloadingUrlMessage = false;
+        sendMessage(
+            receiverId: receiverId,
+            messageDate: DateTime.now().toString(),
+            text: '');
+        _imageMessageUrl = null;
+        update();
+      }).catchError((error) {
+        {
+          print(error.toString());
+        }
+      });
+    }).catchError((error) {
+      print(error.toString());
+    });
+  }
+
+  // NOTE on click close to remove image from chat
+  void removeMessageImage() {
+    _messageImage = null;
+    _imageMessageUrl = null;
     update();
   }
 }
